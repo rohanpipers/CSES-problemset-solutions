1705a54ef151c812514bf0522ac37a5f7f74018c
Rohan Pipersenia <rohanpipers@gmail.com>
2023-07-05 00:33:07 +0900

    weird algo in c

---
+++ introductory_problems/weird_algo.c
@@ -0,0 +1,18 @@
+ #include <stdio.h>
+
+ int main(){
+   int n;
+   scanf("%d", &n);
+   while(n != 1){
+     if(n%2 == 0){
+       printf("%d ", n);
+       n = n/2;
+     }
+     else{
+       printf("%d ", n);
+       n = n*3 + 1;
+     }
+   }
+   printf("%d", n);
+   return 0;
+ }
