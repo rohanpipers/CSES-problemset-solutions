414084205d8a69a3fc664d86f2b025eed9d58c49
Rohan Pipersenia <rohanpipers@gmail.com>
2023-07-06 00:21:10 +0900

    intro problems

---
+++ introductory_problems/IncreasingArray.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+public class IncreasingArray{
+  public static void main(String[] args){
+    Scanner sc = new Scanner(System.in);
+
+    int n = sc.nextInt();
+
+    int arr[] = new int[n];
+    for(int i = 0; i<n; i++){
+      arr[i] = sc.nextInt();
+    }
+
+    long num = 0;
+
+    for(int i = 1; i<n; i++){
+      if(arr[i] < arr[i-1]){
+        num = num + arr[i-1] - arr[i];
+        arr[i] = arr[i-1];
+      }
+    }
+
+    System.out.println(num);
+  }
+}
